22834a586b0ad274df0f40a8fd6178743ebe0622
Lukas Ruebbelke <simpul@gmail.com>
2017-03-28 22:56:33 -0700

    cleaning up

--- src/app/track/track.component.ts
+++ src/app/track/track.component.ts
@@ -1,19 +1,20 @@
 import { ChangeDetectorRef, Component, ElementRef, NgZone, OnInit, ViewChild } from '@angular/core';
 import { D3, D3Service, Timer } from 'd3-ng2-service';
 
-import { MidiInputService } from '../services/pipeline/inputs/midi-input.service';
-import { PipelineService } from '../services/pipeline/pipeline.service';
 import { DomSanitizer } from '@angular/platform-browser';
 import { Observable } from 'rxjs/Observable';
-import { Subscription } from 'rxjs/Subscription';
-import { Observer } from 'rxjs/Observer';
 import { Subject } from 'rxjs';
 
 const noteTransforms = {
-  // TODO Map the rest
+  33: 'A0', 34: 'A#0', 35: 'B0',
+  36: 'C1', 37: 'C#1', 38: 'D1', 39: 'D#1', 40: 'E1', 41: 'F1', 42: 'F#1', 43: 'G1', 44: 'G#1', 45: 'A1', 46: 'A#1', 47: 'B1',
   48: 'C2', 49: 'C#2', 50: 'D2', 51: 'D#2', 52: 'E2', 53: 'F2', 54: 'F#2', 55: 'G2', 56: 'G#2', 57: 'A2', 58: 'A#2', 59: 'B2',
   60: 'C3', 61: 'C#3', 62: 'D3', 63: 'D#3', 64: 'E3', 65: 'F3', 66: 'F#3', 67: 'G3', 68: 'G#3', 69: 'A3', 70: 'A#3', 71: 'B3',
   72: 'C4', 73: 'C#4', 74: 'D4', 75: 'D#4', 76: 'E4', 77: 'F4', 78: 'F#4', 79: 'G4', 80: 'G#4', 81: 'A4', 82: 'A#4', 83: 'B4',
+  84: 'C5', 85: 'C#5', 86: 'D5', 87: 'D#5', 88: 'E5', 89: 'F5', 90: 'F#5', 91: 'G5', 92: 'G#5', 93: 'A5', 94: 'A#5', 95: 'B5',
+  96: 'C6', 97: 'C#6', 98: 'D6', 99: 'D#6', 100: 'E6', 101: 'F6', 102: 'F#6', 103: 'G6', 104: 'G#6', 105: 'A6', 106: 'A#6', 107: 'B6',
+  108: 'C7', 109: 'C#7', 110: 'D7', 111: 'D#7', 112: 'E7', 113: 'F7', 114: 'F#7', 115: 'G7', 116: 'G#7', 117: 'A7', 118: 'A#7', 119: 'B7',
+  120: 'C8'
 };
 
 declare const navigator: any;
@@ -28,7 +29,7 @@ export class TrackComponent implements OnInit {
   midiInputs: Array<any>;
   notes: Array<any> = [];
   currentNote;
-  noteTransforms = Object.keys(noteTransforms).map((key)=>{ return {frequency: key, note: noteTransforms[key]}});
+  noteTransforms = Object.keys(noteTransforms).map((key) => { return {frequency: key, note: noteTransforms[key]}});
 
   audioContext: AudioContext = window['theAudioContext'];
   audioInput = null;
@@ -53,9 +54,7 @@ export class TrackComponent implements OnInit {
   private parentNativeElement: any;
 
 
-  constructor(private midiInputService: MidiInputService,
-              private pipelineService: PipelineService,
-              private cd: ChangeDetectorRef,
+  constructor(private cd: ChangeDetectorRef,
               private sanitizer: DomSanitizer,
               element: ElementRef,
               d3Service: D3Service) {
@@ -67,39 +66,67 @@ export class TrackComponent implements OnInit {
   ngOnInit() {
     this.doSomethingClever();
 
-    // this.doSomethingLoud();
-    // this.doSomethingViolent();
-    // this.doSomethingPretty();
-    // this.doSomethingRandom();
-    // this.doSomethingMicrophony();
     // this.doSomethingVisual();
+
+    // D3 STUFF
+    // this.doSomethingPretty();
+    // this.doSomethingViolent();
+    // this.doSomethingRandom();
+
+    // WAVERIDER STUFF
+    // this.doSomethingWavy();
+    // this.doSomethingMicrophony();
   }
 
-  getAdjustedNoteHeight(note) {
-    return `${50 + note.pressure / 2.5}%`;
+  // -------------------------------------------------------------------
+  // RxJS MIDI with TONEJS
+  // -------------------------------------------------------------------
+  private doSomethingClever() {
+    const midiAccess$ = Observable.fromPromise(navigator.requestMIDIAccess());
+
+    const stateStream$ = midiAccess$.flatMap(access => this.stateChangeAsObservable(access));
+
+    const inputStream$ = midiAccess$
+      .map((midi: any) => midi.inputs.values().next().value)
+    ;
+
+    const messages$ = inputStream$
+      .filter(input => input !== undefined)
+      .flatMap(input => this.midiMessageAsObservable(input))
+      .map((message: any) => ({
+        // Collect relevant data from the message
+        // See for example http://www.midi.org/techspecs/midimessages.php
+        status: message.data[0] & 0xf0,
+        data: [
+          message.data[1],
+          message.data[2],
+        ],
+      }))
+    ;
+
+    stateStream$.subscribe(state => console.log('STATE CHANGE EVENT', state));
+
+    messages$.subscribe(note => {
+      this.notes = this.notes.concat(note);
+      this.currentNote = noteTransforms[note.data[0]];
+
+      this.processNoteTransforms(note);
+      this.midiMessageReceived(note);
+      this.cd.detectChanges();
+    });
   }
 
-  private initSynth() {
-    return new Tone.MonoSynth({
-      'portamento': 0.01,
-      'oscillator': {
-        'type': 'square'
-      },
-      'envelope': {
-        'attack': 0.005,
-        'decay': 0.2,
-        'sustain': 0.4,
-        'release': 1.4,
-      },
-      'filterEnvelope': {
-        'attack': 0.005,
-        'decay': 0.1,
-        'sustain': 0.05,
-        'release': 0.8,
-        'baseFrequency': 300,
-        'octaves': 4
-      }
-    }).toMaster();
+  private processNoteTransforms(note) {
+    const frequency = note.data[0] + ''; // hack to do strict equality comparison
+    const pressure  = note.data[1];
+
+    this.noteTransforms
+      .forEach(n => {
+        if (n.frequency === frequency) {
+          n['active']   = pressure > 0;
+          n['pressure'] = pressure;
+        }
+      });
   }
 
   private midiMessageReceived(message: any) {
@@ -135,121 +162,36 @@ export class TrackComponent implements OnInit {
     return source.asObservable();
   }
 
-  private doSomethingClever() {
-    const midiAccess$ = Observable.fromPromise(navigator.requestMIDIAccess());
-
-    const stateStream$ = midiAccess$.flatMap(access => this.stateChangeAsObservable(access));
-
-    const inputStream$ = midiAccess$
-      .map((midi: any) => midi.inputs.values().next().value)
-    ;
-
-    const messages$ = inputStream$
-      .filter(input => input !== undefined)
-      .flatMap(input => this.midiMessageAsObservable(input))
-      .map((message: any) => ({
-        // Collect relevant data from the message
-        // See for example http://www.midi.org/techspecs/midimessages.php
-        status: message.data[0] & 0xf0,
-        data: [
-          message.data[1],
-          message.data[2],
-        ],
-      }))
-    ;
-
-    stateStream$.subscribe(state => console.log('STATE CHANGE EVENT', state));
-
-    messages$.subscribe(note => {
-      const frequency = note.data[0];
-      const pressure  = note.data[1];
-
-      // JG ~ may be a better way to do this
-      this.noteTransforms
-        .forEach(n => {
-          if (n.frequency === frequency) {
-            n['active']   = pressure > 0;
-            n['pressure'] = pressure;
-          }
-        });
-      this.midiMessageReceived(note);
-      this.cd.detectChanges();
-    });
-
-    // Stream of note on messages
-    // const notes$ = messages$.filter(message => message.status === 144);
-
-    // Stream of control change messages
-    // const controls$ = messages$.filter(message => message.status === 176);
-
-    // notes$.subscribe(message => {
-    //   console.log(`Note ${message.data[0]} triggered with velocity ${message.data[1]}`);
-    // });
-
-    // controls$.subscribe(message => {
-    //   console.log(`Control ${message.data[0]} changed with value ${message.data[1]}`);
-    // });
+  private initSynth() {
+    return new Tone.MonoSynth({
+      'portamento': 0.01,
+      'oscillator': {
+        'type': 'square'
+      },
+      'envelope': {
+        'attack': 0.005,
+        'decay': 0.2,
+        'sustain': 0.4,
+        'release': 1.4,
+      },
+      'filterEnvelope': {
+        'attack': 0.005,
+        'decay': 0.1,
+        'sustain': 0.05,
+        'release': 0.8,
+        'baseFrequency': 300,
+        'octaves': 4
+      }
+    }).toMaster();
   }
 
-  private doSomethingPretty() {
-    let d3 = this.d3;
-
-    // set the dimensions and margins of the graph
-    let margin = {top: 20, right: 20, bottom: 30, left: 50},
-      width = 960 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom,
-      angles = d3.range(0, 2 * Math.PI, Math.PI / 200);
-
-    let svg = d3.select(this.parentNativeElement)
-      .append('svg')
-      .attr('width', width + margin.left + margin.right)
-      .attr('height', height + margin.top + margin.bottom);
-
-    this.path = svg.append('g')
-      .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
-      .attr('fill', 'none')
-      .attr('stroke-width', 10)
-      .attr('stroke-linejoin', 'round')
-      .selectAll('path')
-      .data(['cyan', 'magenta', 'yellow'])
-      .enter().append('path')
-      .attr('stroke', function (d) {
-        return d;
-      })
-      .style('mix-blend-mode', 'darken')
-      .datum(function (d, i) {
-        return d3.radialLine()
-          .curve(d3.curveLinearClosed)
-          .angle((a: any) => a)
-          .radius((a: any) => {
-            let t = d3.now() / 1000;
-            return 200 + Math.cos(a * 8 - i * 2 * Math.PI / 3 + t) * Math.pow((1 + Math.cos(a - t)) / 2, 3) * 32;
-          });
-      });
-
-    d3.timer(() => this.path.attr('d', (d: any) => d(angles)));
+  getAdjustedNoteHeight(note) {
+    return `${50 + note.pressure / 2.5}%`;
   }
 
-  private doSomethingLoud() {
-    this.midiInputService.elaborateDevices()
-      .subscribe(midiInputs => this.midiInputs = midiInputs);
-
-    this.pipelineService.synthStream$
-      .subscribe(note => {
-        // this.notes.push(note);
-        this.currentNote = noteTransforms[note['note']];
-        this.cd.detectChanges();
-      });
-  }
-
-  private connectSource(event) {
-    this.pipelineService.begin(event.target.value);
-
-    let device = this.midiInputs.find((input: any) => input.id === event.target.value);
-    this.midiInputService.beginMidiInput(device);
-  }
-
-
+  // -------------------------------------------------------------------
+  // VISUALIZE AND SAVE AUDIO
+  // -------------------------------------------------------------------
   doSomethingVisual() {
     let mediaConstraints: any = {
       audio: {
@@ -374,32 +316,6 @@ export class TrackComponent implements OnInit {
     this.inputPoint.connect(this.analyserNode);
     this.analyserNode.fftSize = 2048;
     this.inputPoint.connect(this.audioContext.destination);
-
-
-    // Create an AudioNode from the stream.
-    // this.realAudioInput = this.audioContext.createMediaStreamSource(stream);
-    // this.audioInput = this.realAudioInput;
-    // this.audioInput.connect(this.inputPoint);
-
-    // audioInput = convertToMono( input );
-
-    // TEST
-    // let oscillator = this.audioContext.createOscillator();
-    // oscillator.type = 'sine';
-    // oscillator.frequency.value = 440;
-    // oscillator.start(0);
-    //
-
-    // this.inputPoint = this.audioContext.createGain();
-    // this.inputPoint.gain.value = 1;
-    // oscillator.connect(this.inputPoint);
-
-    // this.audioRecorder = new window['Recorder'](this.inputPoint);
-
-    // this.zeroGain = this.audioContext.createGain();
-    // this.zeroGain.gain.value = 0.0;
-    // this.inputPoint.connect(this.zeroGain);
-    // this.updateAnalysers();
   }
 
   gotBuffers(buffers) {
@@ -457,9 +373,50 @@ export class TrackComponent implements OnInit {
   }
 
   // -------------------------------------------------------------------
-  // IGNORE
+  // D3 VISUALIZATION
   // -------------------------------------------------------------------
+  private doSomethingPretty() {
+    let d3 = this.d3;
 
+    // set the dimensions and margins of the graph
+    let margin = {top: 20, right: 20, bottom: 30, left: 50},
+      width = 960 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom,
+      angles = d3.range(0, 2 * Math.PI, Math.PI / 200);
+
+    let svg = d3.select(this.parentNativeElement)
+      .append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom);
+
+    this.path = svg.append('g')
+      .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
+      .attr('fill', 'none')
+      .attr('stroke-width', 10)
+      .attr('stroke-linejoin', 'round')
+      .selectAll('path')
+      .data(['cyan', 'magenta', 'yellow'])
+      .enter().append('path')
+      .attr('stroke', function (d) {
+        return d;
+      })
+      .style('mix-blend-mode', 'darken')
+      .datum(function (d, i) {
+        return d3.radialLine()
+          .curve(d3.curveLinearClosed)
+          .angle((a: any) => a)
+          .radius((a: any) => {
+            let t = d3.now() / 1000;
+            return 200 + Math.cos(a * 8 - i * 2 * Math.PI / 3 + t) * Math.pow((1 + Math.cos(a - t)) / 2, 3) * 32;
+          });
+      });
+
+    d3.timer(() => this.path.attr('d', (d: any) => d(angles)));
+  }
+
+  // -------------------------------------------------------------------
+  // D3 RANDOM CHART
+  // -------------------------------------------------------------------
   private doSomethingRandom() {
     let d3 = this.d3;
     let canvas = document.querySelector('canvas'),
@@ -531,6 +488,9 @@ export class TrackComponent implements OnInit {
     }
   }
 
+  // -------------------------------------------------------------------
+  // D3 CHART
+  // -------------------------------------------------------------------
   private doSomethingViolent() {
     let data = [
       {date: '1-May-12', close: '58.13'},
@@ -616,6 +576,9 @@ export class TrackComponent implements OnInit {
       .call(d3.axisLeft(y));
   }
 
+  // -------------------------------------------------------------------
+  // WAVE SURFER BASIC
+  // -------------------------------------------------------------------
   private doSomethingWavy() {
     let wavesurfer = window['WaveSurfer'].create({
       container: '#waveform',
@@ -626,6 +589,9 @@ export class TrackComponent implements OnInit {
     wavesurfer.load('assets/audio/TellMeHow.mp3');
   }
 
+  // -------------------------------------------------------------------
+  // WAVE SURFER MIC
+  // -------------------------------------------------------------------
   private doSomethingMicrophony() {
     let wavesurfer = window['WaveSurfer'].create({
       container: '#waveform',
